dcb266850ff6e9c602e5ced8899932101bcd8ddd
Mike James <m.l.james@me.com>
2015-11-17 15:45:31 +0000

    added output option

--- importer.js
+++ importer.js
@@ -2,8 +2,9 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-d, --data [value]', 'input data json')
-  .option('-dict, --dictionary [value]', 'input data dictionary, mapping file of sourceKey and newKey')
+  .option('-i, --data [value]', 'input data json')
+  .option('-d, --dictionary [value]', 'input data dictionary, mapping file of sourceKey and newKey')
+  .option('-o, --output [output]', 'output filename')
   .parse(process.argv);
  
 if(program.data && program.dictionary){
@@ -14,6 +15,7 @@ if(program.data && program.dictionary){
   const data = require(dataPath),
     dictionary = require(dictionaryPath);
 
+  
 
   console.log(data);
   console.log(dictionary);
