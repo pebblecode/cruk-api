a4a75e8b9a89365cb0154702fb1acb41db079067
Jonathan King <jonathan@pebblecode.com>
2015-11-17 12:21:04 +0000

    added cities.json and db

---
+++ test/db-spec.js
@@ -0,0 +1,8 @@
+'use strict';
+// const expect = require('chai').expect;
+// const moment = require('moment');
+import Hospital from '../src/db.js'
+
+// const Hospital = require('../lib/db.js').Hospital;
+
+// Hospital.find({}, (err, coll) => console.log(err, coll));
