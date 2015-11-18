7c27498c1f8265c969b3e920387b4687b10bea6b
Mike James <m.l.james@me.com>
2015-11-18 09:29:28 +0000

    full transform and load script

--- go.sh
+++ go.sh
@@ -2,3 +2,4 @@ node transformer.js -i ./cancerdata.json -d ./cancerfields.json -c
 node transformer.js -i ./patientsregistered.json -d ./cancerfields.json
 node transformer.js -i ./stg1and2detection.json -d ./cancerfields.json -c
 node joiner.js -c ./cancerdata-processed.json -p ./patientsregistered-processed.json -s ./stg1and2detection-processed.json
+npm run importer
